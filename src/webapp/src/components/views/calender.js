a5d7f4525bd7d6fe5f35ba7e8642895427699b9e
Sai Kumar Manakan <saikumar.manakan@gmail.com>
2020-08-15 15:31:58 -0500

    Full year view

--- src/webapp/src/components/views/calender.js
+++ src/webapp/src/components/views/calender.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import SingleMonthView from "../tools/calender/SingleMonthView";
 import ThreeMonthsView from "../tools/calender/ThreeMonthsView";
+import FullYearView from "../tools/calender/FullYearView";
 
 class Calender extends Component {
     render = () => {
@@ -8,6 +9,7 @@ class Calender extends Component {
             <div>
                 <SingleMonthView key={'single-month-view'} year={2020} currentMonth={0} showNavigation={true}/>
                 <ThreeMonthsView key={'three-month-view'} year={2020} startMonth={0} showNavigation={true}/>
+                <FullYearView/>
             </div>
         );
     }
